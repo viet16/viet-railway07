00d82090276905e135beb2a40ac47aa33380969c
viet16 <77057677+viet16@users.noreply.github.com>
2021-01-31 16:09:32 +0700

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,12 @@
+package entities;
+import java.time.LocalDate;
+
+public class Account {
+ int accountId ; 
+ String emall ;
+ String userName ;
+ String  FullName ;
+ Department departmentId ;
+ Position positionId ;
+ LocalDate createDate ;
+}
